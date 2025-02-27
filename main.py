86f7e41a182f1f1266312bdd5bccf76f69e3ada0
Milos Gavranovic <mgavranovic7@gmail.com>
2025-02-27 12:13:36 +0100

    Add basic game loop and screen

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_WIDTH))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, color=(000, 000, 000))
+
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
