626cc82302090f1e3a77f2633fe64498b19d2f57
Milos Gavranovic <mgavranovic7@gmail.com>
2025-02-28 08:48:52 +0100

    Draw player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,11 +9,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2   
+
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
